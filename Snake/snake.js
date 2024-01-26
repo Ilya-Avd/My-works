c4fe8c2aa3cae066d04e43f45d285a2d9a94028e
Ilya <ilya.avdankkin@gmail.com>
2024-01-26 15:19:08 +0300

    last

---
+++ Snake/snake.js
@@ -0,0 +1,115 @@
+function snakeGame(complexity){
+    document.getElementById('but').style.visibility = 'hidden'; 
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+const ground = new Image();
+ground.src = "/7СЕМ/map.png";
+
+const foodImg = new Image();
+foodImg.src = "/7СЕМ/food.png";
+
+let box = 32;
+
+let score = 0;
+
+let food = {
+	x: Math.floor((Math.random() * 17 + 1)) * box,
+	y: Math.floor((Math.random() * 15 + 3)) * box,
+};
+
+let snake = [];
+snake[0] = {
+	x: 9 * box,
+	y: 10 * box
+};
+
+document.addEventListener("keydown", direction);
+
+let dir;
+
+function direction(event) {
+	switch (true){
+        case event.keyCode == 37 && dir != "right":
+		    dir = "left";
+        break
+	     case event.keyCode == 38 && dir != "down":
+		    dir = "up";
+         break
+	      case event.keyCode == 39 && dir != "left":
+		    dir = "right";
+          break
+	        case event.keyCode == 40 && dir != "up":
+		     dir = "down";
+            break
+}
+}
+
+
+function Gameover(){
+    ctx.fillStyle = "white";
+    ctx.font = "50px Arial";
+    ctx.fillText('Game Over', 190, 304);
+    document.getElementById('but').style.visibility = 'visible'; 
+    clearInterval(game)
+   ;
+}
+
+
+
+function eatTail(head, arr) {
+	for(let i = 0; i < arr.length; i++) {
+		if(head.x == arr[i].x && head.y == arr[i].y)
+			Gameover();
+	}
+    
+}
+
+function drawGame() {
+	ctx.drawImage(ground, 0, 0);
+
+	ctx.drawImage(foodImg, food.x, food.y);
+
+	for(let i = 0; i < snake.length; i++) {
+		ctx.fillStyle = i == 0 ? "green" : "red";
+		ctx.fillRect(snake[i].x, snake[i].y, box, box);
+        
+	}
+
+	ctx.fillStyle = "white";
+	ctx.font = "50px Arial";
+	ctx.fillText(score, box * 2.5, box * 1.7);
+
+	let snakeX = snake[0].x;
+	let snakeY = snake[0].y;
+
+	if(snakeX == food.x && snakeY == food.y) {
+		score++;
+		food = {
+			x: Math.floor((Math.random() * 17 + 1)) * box,
+			y: Math.floor((Math.random() * 15 + 3)) * box,
+		};
+	} else
+		snake.pop();
+
+	if(snakeX < box || snakeX > box * 17
+		|| snakeY < 3 * box || snakeY > box * 17){
+         Gameover();
+        }
+	if(dir == "left") snakeX -= box;
+	if(dir == "right") snakeX += box;
+	if(dir == "up") snakeY -= box;
+	if(dir == "down") snakeY += box;
+
+	let newHead = {
+		x: snakeX,
+		y: snakeY
+	};
+
+	eatTail(newHead, snake);
+
+	snake.unshift(newHead);
+}
+
+let game = setInterval(drawGame, complexity);
+}
